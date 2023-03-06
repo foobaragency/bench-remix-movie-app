995a4fbaafd7ebd9e28cd90999b1ecee9ff64fe6
Peter Mbanugo <peter@foobar.agency>
2023-03-06 13:21:19 +0100

    add link for MovieCard

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -1,8 +1,8 @@
+import { Link } from "@remix-run/react";
+import { IMAGE_BASE_URL } from "~/config";
 import type { Movie } from "~/tmdb-client";
 
 type ImageWidth = "185" | "342" | "500" | "780";
-
-const IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 
 export default function Index({
   movie,
@@ -12,22 +12,24 @@ export default function Index({
   imageWidth: ImageWidth;
 }) {
   return (
-    <figure className="mx-auto p-8 md:p-0">
-      <img
-        className="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
-        // className="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto md:w-48"
-        src={`${IMAGE_BASE_URL}/w${imageWidth}/${movie.poster_path}`}
-        alt={movie.title}
-        width={imageWidth}
-      />
-      <div className="p-1.5">
-        <figcaption className="text-slate-900 dark:text-slate-700">
-          <div className="text-sm font-bold">{movie.title}</div>
-          <div className="text-xs font-thin">
-            {new Date(movie.release_date).toDateString()}
-          </div>
-        </figcaption>
-      </div>
-    </figure>
+    <Link to={`/people/${movie.id}`}>
+      <figure className="mx-auto p-8 md:p-0">
+        <img
+          className="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto"
+          // className="mx-auto rounded-xl shadow-md  shadow-slate-400 md:h-auto md:w-48"
+          src={`${IMAGE_BASE_URL}/w${imageWidth}/${movie.poster_path}`}
+          alt={movie.title}
+          width={imageWidth}
+        />
+        <div className="p-1.5">
+          <figcaption className="text-slate-900 dark:text-slate-700">
+            <div className="text-sm font-bold">{movie.title}</div>
+            <div className="text-xs font-thin">
+              {new Date(movie.release_date).toDateString()}
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </Link>
   );
 }
