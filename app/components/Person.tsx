cd35b36175b65591e8340b1ddb6f7c8de456e96b
Peter Mbanugo <peter@foobar.agency>
2023-03-08 19:57:25 +0100

    fix svg rules

--- app/components/Person.tsx
+++ app/components/Person.tsx
@@ -23,9 +23,9 @@ export default function Index({ person }: { person: CastMember | CrewMember }) {
               className="flex w-full items-center"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
                 transform="scale(1.333) translate(-3 3)"
               />
             </svg>
